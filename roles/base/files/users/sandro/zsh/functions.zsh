4c722f10b9b53da145bda9152d1972748d30e802
sandro-roth <sandro.roth@hotmail.de>
2026-03-15 16:13:04 +0100

    make extract function more robust

--- roles/base/files/users/sandro/zsh/functions.zsh
+++ roles/base/files/users/sandro/zsh/functions.zsh
@@ -1,25 +1,26 @@
 # Easily extract archives
-extract () {
-  if [ -f $1 ] ; then
-    case $1 in
-      *.tar.bz2)   tar xjvf $1    ;;
-      *.tar.gz)    tar xzvf $1    ;;
-      *.tar.xz)    tar xvf $1    ;;
-      *.bz2)       bzip2 -d $1    ;;
-      *.rar)       unrar2dir $1    ;;
-      *.gz)        gunzip $1    ;;
-      *.tar)       tar xf $1    ;;
-      *.tbz2)      tar xjf $1    ;;
-      *.tgz)       tar xzf $1    ;;
-      *.zip)       unzip2dir $1     ;;
-      *.Z)         uncompress $1    ;;
-      *.7z)        7z x $1    ;;
-      *.ace)       unace x $1    ;;
-      *)           echo "'$1' cannot be extracted via extract()"   ;;
-    esac
-  else
+extract() {
+  [ -f "$1" ] || {
     echo "'$1' is not a valid file"
-  fi
+    return 1
+  }
+
+  case "$1" in
+    *.tar.bz2) tar xjvf "$1" ;;
+    *.tar.gz)  tar xzvf "$1" ;;
+    *.tar.xz)  tar xvf "$1" ;;
+    *.bz2)     bzip2 -d "$1" ;;
+    *.rar)     unrar2dir "$1" ;;
+    *.gz)      gunzip "$1" ;;
+    *.tar)     tar xf "$1" ;;
+    *.tbz2)    tar xjf "$1" ;;
+    *.tgz)     tar xzf "$1" ;;
+    *.zip)     unzip2dir "$1" ;;
+    *.Z)       uncompress "$1" ;;
+    *.7z)      7z x "$1" ;;
+    *.ace)     unace x "$1" ;;
+    *)         echo "'$1' cannot be extracted via extract()" ;;
+  esac
 }
 
 
